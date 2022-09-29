88052eae21f8b0bfbf43659b266b321a55c9a818
Aziz <88969466+azizvangogh@users.noreply.github.com>
2022-09-29 22:24:32 +0300

    Add files via upload

---
+++ medusa.py
@@ -0,0 +1,17 @@
+# Alert dialog while python
+
+
+
+import pymsgbox
+import os
+import sys
+
+def main():
+    # Sürekli alert dialog
+    while True:
+        pymsgbox.alert("You Are Hacked.", "0x130722250722", "OK")
+        
+    
+
+if __name__ == "__main__":
+    main()
